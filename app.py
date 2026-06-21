394fa85ff93a2e6e4cd4e1d9824b1b562f7457c1
Rusheel Sharma <rusheelhere@gmail.com>
2026-06-21 13:50:21 +0530

    FIX: bypass loopback error & update launch
    Signed-off-by: Rusheel Sharma <rusheelhere@gmail.com>

--- app.py
+++ app.py
@@ -5,4 +5,9 @@ demo = build_ui()
 
 
 if __name__ == "__main__":
-    demo.launch(server_name="0.0.0.0", server_port=7860)
+    demo.launch(
+        server_name="0.0.0.0",
+        server_port=7860,
+        show_error=True,
+        blocked_paths=[]
+    )
